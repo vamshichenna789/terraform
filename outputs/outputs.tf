7199e90bab934820e825248c0897255da0704ece
vamshi <vamshichenna789@gmail.com>
2022-07-05 12:15:52 +0000

    outputs

---
+++ outputs/outputs.tf
@@ -0,0 +1,24 @@
+output "ec2_instance_private_ip" {
+    value = aws_instance.web.*.private_ip
+    description = "EC2 instance Private IP address"
+}
+
+output "ec2_instance_public_ip" {
+    value = aws_instance.web.*.public_ip
+    description = "EC2 instance Public IP address"
+}
+
+output "ec2_instance_public_dns" {
+    value = "http://aws_instance.web.*.public_dns"
+    description = "EC2 instance Private DNS name"
+    #sensitive = true
+}
+
+output "ec2_instance_device_name" {
+  value       = aws_instance.web.*.root_block_device.0.device_name
+  description = "EC2 instance device name"
+}
+
+output "instance_state" {
+  value = aws_instance.web.*.instance_state
+}
---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+az_name = "us-east-1f"
