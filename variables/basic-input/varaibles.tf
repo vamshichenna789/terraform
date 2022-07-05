a01a028fba8b7a4913e7a76d6a4a41b26bc3147e
vamshi <vamshichenna789@gmail.com>
2022-07-05 08:37:11 +0000

    basic varables

---
+++ variables/basic-input/varaibles.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  description = "Region on which resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+variable "az_name" {
+  description = "Availability Zone name should be region defined in aws_region variable"
+  type        = string
+  default     = "us-east-1a"
+}
+variable "ami_name" {
+  description = "Latest AMI ID on us-east-1"
+  type        = string
+  default     = "ami-052efd3df9dad4825"
+}
+variable "inst_type" {
+  description = "Instance type used to launch EC2 Instance"
+  type        = string
+  default     = "t2.micro"
+}
+variable "inst_count" {
+  description = "No of EC2 instances that need to launched"
+  type        = number
+  default     = 1
+}
